8880acfd41e26d7eddf9fb877426119294c07e67
Kirill <krboktv@gmail.com>
2018-10-15 05:00:25 +0300

    Finish data signing

--- software/blockExplorer/ethereum/ethereum.go
+++ software/blockExplorer/ethereum/ethereum.go
@@ -8,88 +8,126 @@ import (
 )
 
 type wallet struct {
-	privateKey *ecdsa.PrivateKey `json:"privateKey"`
-	publicKey *ecdsa.PublicKey   `json:"publicKey"`
+	privateKey privateKey `json:"privateKey"`
+	publicKey  publicKey  `json:"publicKey"`
+}
+
+type privateKey struct {
+	k *ecdsa.PrivateKey
+}
+
+type publicKey struct {
+	k *ecdsa.PublicKey
 }
 
 // Wallet allows to create new ecdsa private key or from existing string private key in []byte format
-func Wallet(pvK ...[]byte) (*wallet, error) {
-	var privateKey *ecdsa.PrivateKey
+func Wallet(_pvK ...[]byte) (*wallet, error) {
+	var pvK *ecdsa.PrivateKey
 	var err error
 
-	if pvK != nil {
-		privateKey, err = crypto.ToECDSA(pvK[0])
+	if _pvK != nil {
+		pvK, err = crypto.ToECDSA(_pvK[0])
 	} else {
-		privateKey, err = crypto.GenerateKey()
+		pvK, err = crypto.GenerateKey()
 	}
 
-	publicKey := &ecdsa.PublicKey{Curve: privateKey.Curve, X: privateKey.X, Y: privateKey.Y}
+	pbK := &ecdsa.PublicKey{Curve: pvK.Curve, X: pvK.X, Y: pvK.Y}
 
 	if err != nil {
 		return nil, err
 	}
 
 	return &wallet{
-		privateKey: privateKey,
-		publicKey: publicKey,
+		privateKey: privateKey{pvK},
+		publicKey: publicKey{pbK},
 	}, nil
 }
 
 // PrivateKey allows to get string private key
-func (w *wallet) PrivateKey() string  {
-	return hex.EncodeToString(w.privateKey.D.Bytes())
+func (w *wallet) PrivateKey() *privateKey  {
+	return &w.privateKey
+}
+
+func (pvK *privateKey) Bytes() []byte {
+	return pvK.k.D.Bytes()
+}
+
+func (pvK *privateKey) Hex() string {
+	return hex.EncodeToString(pvK.k.D.Bytes())
 }
 
 // Allows to get public key associated with private key
-func (w *wallet) PublicKey() string {
-	return hex.EncodeToString(crypto.CompressPubkey(w.publicKey))
+func (w *wallet) PublicKey() *publicKey {
+	return &w.publicKey
+}
+
+func (pubKey *publicKey) Bytes() []byte {
+	return pubKeyToBytes(pubKey)
+}
+
+func (pubKey *publicKey) Hex() string {
+	return utils.BytesToHexString(pubKeyToBytes(pubKey))
+}
+
+func pubKeyToBytes(pubKey *publicKey) []byte {
+	return crypto.FromECDSAPub(pubKey.k)
 }
 
 // Allows to get string address
-func (w *wallet) Address() string  {
-	return crypto.PubkeyToAddress(w.privateKey.PublicKey).Hex()
+func (w *wallet) Address() []byte  {
+	return crypto.PubkeyToAddress(w.privateKey.k.PublicKey).Bytes()
 }
 
-type signature struct {
-	signature []byte			`json:"signature"`
-	hash []byte			`json:"hash"`
+type Signature struct {
+	raw       []byte			`json:"raw"`
+	hash      []byte			`json:"hash"`
+	R     	  []byte			`json:"R"`
+	S         []byte			`json:"S"`
+	V         uint8				`json:"V"`
 }
 
 // Sign allows to sign any byte array via user private key
-func (w *wallet) Sign(data []byte) (*signature, error) {
-	hash := crypto.Keccak256(data)
-	s, err := crypto.Sign(hash, w.privateKey)
+func (w *wallet) Sign(data []byte) (*Signature, error) {
+	dataHash := crypto.Keccak256(data)
+	s, err := crypto.Sign(dataHash, w.privateKey.k)
 	if err != nil {
 		return nil, err
 	}
-	return &signature{
+	return &Signature{
 		s,
-		hash,
+		dataHash,
+		s[:31],
+		s[32:63],
+		uint8(s[64]),
 	}, nil
 }
 
-// ToBytes allows to get byte array of signature
-func (s *signature) ToBytes() []byte {
-	return s.signature
+// Bytes allows to get byte array of signature
+func (s *Signature) Bytes() []byte {
+	return s.raw
 }
 
-// ToHexString allows to get hexadecimal string of signature
-func (s *signature) ToHexString() string {
-	return utils.BytesToHexString(s.signature)
+// HexString allows to get hexadecimal string of signature
+func (s *Signature) HexString() string {
+	return utils.BytesToHexString(s.raw)
 }
 
 // GetHash allows to get signed data hash
-func (s *signature) GetHash() []byte {
+func (s *Signature) GetHash() []byte {
 	return s.hash
 }
 
 // GetPublicKeyFromSignature allows to get Public Key from signature
-func GetPublicKeyFromSignature(s *signature) *ecdsa.PublicKey {
-	pubK, err := crypto.SigToPub(s.hash, s.signature)
-	if err != nil {
-		panic(err.Error())
-	}
+//func GetPublicKeyFromSignature(s *Signature) *publicKey {
+//	pubKey, err := crypto.SigToPub(s.hash, s.raw)
+//	if err != nil {
+//		panic(err.Error())
+//	}
+//
+//	return &publicKey{pubKey}
+//}
 
-	return pubK
+func VerifySignature(pubKey []byte, hash []byte, signature []byte) bool {
+	signatureNoRecoverID := signature[:len(signature)-1]
+	return crypto.VerifySignature(pubKey, hash, signatureNoRecoverID)
 }
-
